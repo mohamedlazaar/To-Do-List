bfe15f3bfff04ce374a25e8f4c09ab3e18ff61dd
mohamedlazaar <45826424+mohamedlazaar@users.noreply.github.com>
2023-10-28 23:04:16 +0100

    script.js

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-/*var semaine = ["lan", "mra", "mer", "jeu", "ven","sam","ddi"]
-
-semaine.pop();
-semaine.push("dim");
-semaine.shift();
-semaine.unshift("lun");
-semaine[1]= "mar";
-console.log(semaine);
-console.log(semaine.length);
-console.log(semaine[4])
-if(semaine.includes("ven")){
-    console.log(true);
-}else{
-    console.log(false)
-}
-console.log(semaine)
-console.log(semaine.indexOf("dim"));
-//week and weekend
-var week = semaine.slice(1,4);
-console.log(week);
-var weekend= semaine.slice(5,7);
-console.log=(weekend);
-
-var fruits= ["Pomme", "Raisin","Banane","Kiwi"];
-document.write(fruits);
-var clientchoice = prompt("choose you fruit");
-var indices = fruits.indexOf(clientchoice);
-if(indices==-1)
-    alert("indisponible");
-else
-    alert("ok");
-
-document.write(indices)
-*/
 let btnincremment= document.getElementById("incremment");
 let btndicremment= document.getElementById("dicremment");
 let todolist = document.getElementById("todolist")
@@ -52,6 +18,7 @@ function addTodoList(){
         li.appendChild(span);
     }
     textt.value = '';
+    saveData();
 }
 todolist.addEventListener('click', function(e){
     if(e.target.tagName ==="LI"){
@@ -61,9 +28,6 @@ todolist.addEventListener('click', function(e){
     }
 }, false)
 
-/*function dicremmentCounter(){
-    counterl--;
-    counter.innerHTML = counterl;
-}*/
-
-
+function saveData(){
+    localStorage.setItem("data", todolist.innerHTML)
+}
