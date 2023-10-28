0803f01fba60487146b2db737c22c775acf241ec
mohamedlazaar <45826424+mohamedlazaar@users.noreply.github.com>
2023-10-28 23:19:03 +0100

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-let btnincremment= document.getElementById("incremment");
-let btndicremment= document.getElementById("dicremment");
 let todolist = document.getElementById("todolist")
-
-let counter = 0;
 let textt = document.getElementById("text");
 
 function addTodoList(){
@@ -31,40 +27,7 @@ todolist.addEventListener('click', function(e){
 function saveData(){
     localStorage.setItem("data", todolist.innerHTML)
 }
-let btnincremment= document.getElementById("incremment");
-let btndicremment= document.getElementById("dicremment");
-let todolist = document.getElementById("todolist")
-
-let counter = 0;
-let textt = document.getElementById("text");
-
-function addTodoList(){
-    if(textt.value === ""){
-        alert("You Must Write Something")
-    }
-    else{
-        let li = document.createElement('li');
-        li.innerHTML += textt.value;
-        todolist.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML="\u00d7";
-        li.appendChild(span);
-    }
-    textt.value = '';
-    saveData();
-}
-todolist.addEventListener('click', function(e){
-    if(e.target.tagName ==="LI"){
-        e.target.classList.toggle("checked");
-    }else if (e.target.tagName === "SPAN"){
-        e.target.parentElement.remove()
-    }
-}, false)
-
-function saveData(){
-    localStorage.setItem("data", todolist.innerHTML)
-}
 function showTask(){
     todolist.innerHTML = localStorage.getItem("data")
 }
-showTask()
+showTask();
